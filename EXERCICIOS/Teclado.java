73cb1ca0e059f2a71f02068db08be0e7893b3bf8
Peterson Cauã Duarte da Silva <125151081+petersoncaua@users.noreply.github.com>
2026-01-29 09:54:36 -0300

    programação orientada a objetos

---
+++ EXERCICIOS/Teclado.java
@@ -0,0 +1,14 @@
+public class Teclado {
+    Teclado teclado = new Teclado();
+
+    String modelo;
+    int ano;
+
+    void teclar() {
+        System.out.println("estou teclando");
+    }
+
+    void desligar() {
+        System.out.println("Teclado desligado, não dá pra teclar");
+    }
+}
